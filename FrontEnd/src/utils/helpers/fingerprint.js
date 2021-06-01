116c81ea3b5a5aacc9b6b4976fa4d46a5d367e98
nosovk <nosovk@gmail.com>
2021-06-01 17:10:59 +0300

    add todos

--- FrontEnd/src/utils/helpers/fingerprint.js
+++ FrontEnd/src/utils/helpers/fingerprint.js
@@ -28,6 +28,7 @@ export default () => {
       };
 
       try {
+        // todo: fingerptint should be fired and LOADED ONLY if no prev value found in localstore
         const components = await Fingerprint2.getPromise(options);
         const values = components.map((component) => component.value);
         console.log('fingerprint hash components', components);
